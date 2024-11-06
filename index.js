8aca4bf4f73dfe31301c466052a58aa82694fd2b
Sherman <sherman.ho@u.nus.edu>
2024-11-06 21:38:40 +0800

    Update logic

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ var randomImage2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelector(".img1").setAttribute("src", randomImage1);
 document.querySelector(".img2").setAttribute("src", randomImage2);
+
+if (randomNumber1 > randomNumber2) { 
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
